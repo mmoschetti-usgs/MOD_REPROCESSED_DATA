6802eac967eb04fbdcd6532589ab9ea7ba19ffcb
Moschetti <mmoschetti@igskcicgltgm067.cr.usgs.gov>
2017-06-28 15:31:35 -0600

    modified mod_reproc...

--- mod_reprocessed_data.c
+++ mod_reprocessed_data.c
@@ -14,12 +14,38 @@
 void delaz_(float *lat1, float *lon1, float *lat2, float *lon2, float *dist, float *az, float *baz);
 int getcols( const char * const line, const char * const delim, char ***out_storage);
 void strip(char *s);
+void assign_cols_flatfile_reprocess(char **columns, float *stLat, float *stLon, float *evMag, float *evLon, float *evLat, float *evDep, int *evYear, int *evMon, int *evDay, int *evHour, int *evMin, float *evSec, char *network, char * stationNm);
 void assign_cols_flatfile(char **columns, float *stLat, float *stLon, float *evMag, float *evLon, float *evLat, float *evDep, int *evYear, int *evMon, int *evDay, int *evHour, int *evMin, float *evSec, char *network, char * stationNm);
 char * replace(char const * const original, char const * const pattern, char const * const replacement );
 int compute_epochTime(int yearIn, int monthIn, int dayIn, int hourIn, int minIn, int secIn);
 
 /*--------------------------------------------------------------------------*/
 void assign_cols_flatfile(char **columns, float *stLat, float *stLon, float *evMag, float *evLon, float *evLat, float *evDep, int *evYear, int *evMon, int *evDay, int *evHour, int *evMin, float *evSec, char *network, char *stationNm)
+/*--------------------------------------------------------------------------*/
+{
+//
+  *stLat=atof(columns[5]);
+  *stLon=atof(columns[6]);
+  *evMag=atof(columns[25]);
+  *evLon=atof(columns[27]);
+  *evLat=atof(columns[26]);
+  *evDep=atof(columns[28]);
+  *evYear=atoi(columns[17]);
+  *evMon=atoi(columns[18]);
+  *evDay=atoi(columns[19]);
+  *evHour=atoi(columns[20]);
+  *evMin=atoi(columns[21]);
+  *evSec=atof(columns[22]);
+  network=strcpy(network,columns[1]);
+  stationNm=strcpy(stationNm,columns[2]);
+//  fprintf(stderr,"assign_cols_flatfile, evMag/stLat: %f %f\n", *evMag, *stLat);
+//  fprintf(stderr,"assign_cols_flatfile, network/stationNm: %s %s\n", network, stationNm);
+
+}
+
+
+/*--------------------------------------------------------------------------*/
+void assign_cols_flatfile_reprocess(char **columns, float *stLat, float *stLon, float *evMag, float *evLon, float *evLat, float *evDep, int *evYear, int *evMon, int *evDay, int *evHour, int *evMin, float *evSec, char *network, char *stationNm)
 /*--------------------------------------------------------------------------*/
 {
 //
@@ -61,7 +87,7 @@ void strip(char *s)
 int main (int argc, char *argv[])
 /*--------------------------------------------------------------------------*/
 {
-  FILE *fpFlatFileMod, *fpFlatFile, *fpAddToFlatFile;
+  FILE *fpFlatFileMod, *fpFlatFile, *fpReprocessFlatFile;
   int cnt, cntMod=0, cntOrig=0, cnt1, cnt2, cnt3;
   int hlines;
   int cols_found, diffSec, minDiff;
@@ -72,7 +98,7 @@ int main (int argc, char *argv[])
   float stLon, stLat, evMag, evLon, evLat, evDep, evSec;
   float stLon2, stLat2, evMag2, evLon2, evLat2, evDep2, evSec2;
   float dist, dist2, az, baz, mindist;
-  char addToFlatFile[200], flatFile[200], flatFileMod[200];
+  char reprocessedFlatFile[200], flatFile[200], flatFileMod[200];
   char buff[BUFFLEN], buff2[BUFFLEN], buffFin[BUFFLEN];
   char network[5], stationNm[10], network2[5], stationNm2[10];
   char **columns;
@@ -83,12 +109,12 @@ int main (int argc, char *argv[])
 
 /* CHECK INPUT ARGUMENTS */
   if ( argc != 4 ) {
-    fprintf(stderr,"USAGE: %s [flatfile] [additional flatfile] [output flatfile]\n", argv[0]);
+    fprintf(stderr,"USAGE: %s [flatfile] [reprocessed flatfile] [output flatfile]\n", argv[0]);
     fprintf(stderr,"Relocated catalog file, e.g., emm_c2_OK_KS_201702_add_all2.csv\n");
     exit(1);
   }
   sscanf(argv[1],"%s", flatFile);
-  sscanf(argv[2],"%s", addToFlatFile);
+  sscanf(argv[2],"%s", reprocessedFlatFile);
   sscanf(argv[3],"%s", flatFileMod);
 
 // Open flatfiles
@@ -100,14 +126,14 @@ int main (int argc, char *argv[])
   else {
     fprintf(stderr,"Opened flatfile, %s\n", flatFile);
   }
-  if ((fpAddToFlatFile = fopen(addToFlatFile, "r")) == NULL) {
-    fprintf(stderr,"Could not open flatfile, %s\n", addToFlatFile);
+  if ((fpReprocessFlatFile = fopen(reprocessedFlatFile, "r")) == NULL) {
+    fprintf(stderr,"Could not open flatfile, %s\n", reprocessedFlatFile);
     exit(0);
   }
   else {
-    fprintf(stderr,"Opened add to flatfile, %s\n", addToFlatFile);
+    fprintf(stderr,"Opened add to flatfile, %s\n", reprocessedFlatFile);
   }
-//  fclose(fpAddToFlatFile);
+//  fclose(fpReprocessFlatFile);
   fpFlatFileMod = fopen(flatFileMod, "w");
   fprintf(stderr,"Writing to flatfile, %s\n\n", flatFileMod);
 
@@ -137,19 +163,19 @@ int main (int argc, char *argv[])
     assign_cols_flatfile(columns, &stLat, &stLon, &evMag, &evLon, &evLat, &evDep, &evYear, &evMon, &evDay, &evHour, &evMin, &evSec, network, stationNm);
     epochTimeFlatFile=compute_epochTime(evYear,evMon,evDay,evHour,evMin,(int)evSec);
 
-// LOOP THROUGH ALL LINES IN MODIFIED FLATFILE
+// LOOP THROUGH ALL LINES IN REPROCESSED FLATFILE
     objMisfit=5;
 // header lines
-    fgets(buff2,BUFFLEN,fpAddToFlatFile);
-    fgets(buff2,BUFFLEN,fpAddToFlatFile);
-    while( fgets(buff2,BUFFLEN,fpAddToFlatFile) ) {
+    fgets(buff2,BUFFLEN,fpReprocessFlatFile);
+    fgets(buff2,BUFFLEN,fpReprocessFlatFile);
+    while( fgets(buff2,BUFFLEN,fpReprocessFlatFile) ) {
 //      strip(buff2);
       buff2[strcspn(buff2, "\r\n")] = 0;
       columns2 = NULL;
       cols_found = getcols(buff2, delim, &columns2);
 //fprintf(stderr,"from %s",addToFlatFile);
 //fprintf(stderr,"%s\n",buff2);
-      assign_cols_flatfile(columns2, &stLat2, &stLon2, &evMag2, &evLon2, &evLat2, &evDep2, &evYear2, &evMon2, &evDay2, &evHour2, &evMin2, &evSec2, network2, stationNm2);
+      assign_cols_flatfile_reprocess(columns2, &stLat2, &stLon2, &evMag2, &evLon2, &evLat2, &evDep2, &evYear2, &evMon2, &evDay2, &evHour2, &evMin2, &evSec2, network2, stationNm2);
       free(columns2);
       delaz_(&stLat,&stLon,&stLat2,&stLon2,&dist,&az,&baz);
       delaz_(&evLat,&evLon,&evLat2,&evLon2,&dist2,&az,&baz);
@@ -172,7 +198,7 @@ int main (int argc, char *argv[])
 //      }
     }
 // rewind to beginning of file for next iteration through original flatfile
-    rewind(fpAddToFlatFile);
+    rewind(fpReprocessFlatFile);
 
 // WRITE MODIFIED FLATFILE
     if ( objMisfit < 1 ) {
@@ -215,7 +241,7 @@ int main (int argc, char *argv[])
 
 // close files
   fclose(fpFlatFile);
-  fclose(fpAddToFlatFile);
+  fclose(fpReprocessFlatFile);
   fclose(fpFlatFileMod);
 
   return 0;
